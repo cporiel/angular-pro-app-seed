ae05258aec19761fef97497fa35ba7d9b853f9ad
cporiel <corentin.poriel@insa-rouen.fr>
2019-06-06 10:23:52 +0200

    architecture application 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { Store } from 'store';
 
 // feature modules
-
+import { AuthModule } from '../auth/auth.module'
 // containers
 import { AppComponent } from './containers/app/app.component';
 
@@ -17,7 +17,8 @@ export const ROUTES: Routes = [];
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    AuthModule
   ],
   declarations: [
     AppComponent
@@ -30,3 +31,13 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+ 
+// var firebaseConfig = {
+//   apiKey: "AIzaSyDgEgCT-WxlLTT0x4SN2e1xjtKysjK4T9o",
+//   authDomain: "formationskwarel.firebaseapp.com",
+//   databaseURL: "https://formationskwarel.firebaseio.com",
+//   projectId: "formationskwarel",
+//   storageBucket: "formationskwarel.appspot.com",
+//   messagingSenderId: "149405741495",
+//   appId: "1:149405741495:web:8626efc3c161bae3"
+// };
